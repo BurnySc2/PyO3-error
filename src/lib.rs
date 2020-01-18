97e2ec0bfbc0db29bec1e7ec5bf7cf6890a24375
BuRny <gamingburny@gmail.com>
2020-01-18 20:59:00 +0100

    Push lib

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use pyo3::prelude::*;
+use pyo3::types::PyAny;
+use pyo3::wrap_pyfunction;
+use pyo3::PyObjectProtocol;
+
+#[pyclass]
+struct MyClass {
+    num: i32,
+}
+
+#[pymethods]
+impl MyClass {
+//    #[new]
+//    fn new(obj: &PyRawObject, num: i32) {
+//        obj.init({ MyClass { num } });
+//    }
+}
+
+#[pymodule]
+fn my_library(_py: Python, m: &PyModule) -> PyResult<()> {
+    // Classes to be exported
+    m.add_class::<MyClass>()?;
+    Ok(())
+}
