c942c61224916b71bc9a4b54115861b237112175
BuRny <gamingburny@gmail.com>
2020-08-10 21:45:31 +0200

    Update pyo3 and rustup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
 use pyo3::prelude::*;
-use pyo3::types::PyAny;
-use pyo3::wrap_pyfunction;
-use pyo3::PyObjectProtocol;
 
 #[pyclass]
 struct MyClass {
@@ -14,11 +11,39 @@ impl MyClass {
     fn new(num: i32) -> Self {
         MyClass { num }
     }
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!(
+            "MyClass ( num: {:?} )",
+            self.num
+        ))
+    }
+}
+
+#[pyclass]
+struct Point3d {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+#[pymethods]
+impl Point3d {
+    #[new]
+    fn new(x: i32, y: i32, z: i32) -> Self {
+        Point3d { x, y, z }
+    }
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!(
+            "Point3d ( x: {:?}, y: {:?}, z: {:?} )",
+            self.x, self.y, self.z
+        ))
+    }
 }
 
 #[pymodule]
 fn my_library(_py: Python, m: &PyModule) -> PyResult<()> {
     // Classes to be exported
     m.add_class::<MyClass>()?;
+    m.add_class::<Point3d>()?;
     Ok(())
 }
